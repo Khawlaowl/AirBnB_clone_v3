68cf6685f91a816f96d7bb54c461c5a97e201824
Notyasser <showhad55@gmail.com>
2024-04-03 01:24:06 +0200

    updt

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-"""Create a Route `/status` on the object app_views."""
+"""index"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.user import User
+from models.place import Place
 from models.state import State
 from models.city import City
-from models.place import Place
-from models.review import Review
 from models.amenity import Amenity
-from models.user import User
+from models.review import Review
+
+classes = {"users": "User", "places": "Place", "states": "State",
+           "cities": "City", "amenities": "Amenity",
+           "reviews": "Review"}
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def jsonstatus():
-    """the return ok status"""
-    return jsonify(status='OK')
+@app_views.route('/status', methods=['GET'])
+def status():
+    ''' routes to status page '''
+    return jsonify({'status': 'OK'})
 
 
-@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
-def getstats():
-    """endpoint that retrives the number of each obcts by type"""
-    dicreturn = {'amenities': storage.count('Amenity'),
-                 'cities': storage.count('City'),
-                 'places': storage.count('Place'),
-                 'reviews': storage.count('Review'),
-                 'states': storage.count('State'),
-                 'users': storage.count('User')}
-    return jsonify(dicreturn)
+@app_views.route('/stats', methods=['GET'])
+def count():
+    '''retrieves the number of each objects by type'''
+    count_dict = {}
+    for cls in classes:
+        count_dict[cls] = storage.count(classes[cls])
+    return jsonify(count_dict)
